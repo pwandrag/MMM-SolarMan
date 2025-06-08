b9a27f686f778eac31045a330ffb4b101e76a67e
Paul Wandrag <paul.wandrag@dometic.com>
2025-06-08 23:45:52 +0200

    Refactor MMM-SolarMan module: update CSS for progress bar, enhance JS data handling, and upgrade dependencies

--- MMM-SolarMan.js
+++ MMM-SolarMan.js
@@ -8,8 +8,6 @@
  *
  */
 
-const { createElement } = require("react");
-
 Module.register("MMM-SolarMan",{
 	// Default module config.
 	defaults: {	  
@@ -24,7 +22,7 @@ Module.register("MMM-SolarMan",{
 	// Define required scripts. Chart.js needed for the graph.
 	getScripts: function() {
 	  return [
-		'modules/MMM-SolarMan/node_modules/chart.js/dist/Chart.bundle.js',
+		'modules/MMM-SolarMan/node_modules/chart.js/dist/Chart.js',
 		'moment.js'
 	  ];
 	},
@@ -69,21 +67,17 @@ Module.register("MMM-SolarMan",{
 		  msgStats.innerHTML = this.translate("NODATA");
 		  return;
 		} else {
-			console.log("Going to draw Day chart with the following data: ");
-			console.log(payload.data);
-
-		  this.drawSummaryTable(payload.data);
+		  	this.drawSummaryTable(payload.data);
+			return;
 		}
 	} else if (notification == "SOLARMAN_DAY_DETAIL") {
 		// no data received from node_helper.js
 		if (!payload.data || payload.data.length == 0) {
-		  msgStats.innerHTML = this.translate("NODATA");
-		  return;
+		  	msgStats.innerHTML = this.translate("NODATA");
+		  	return;
 		} else {
-			console.log("Going to draw Month chart with the following data: ");
-			console.log(payload.data);
-
 			this.drawDayChart(payload.data);
+			return;
 		}
 	  }
 	},
@@ -103,7 +97,7 @@ Module.register("MMM-SolarMan",{
 		headerText.style = "margin: 0px 0px 0px 15px;";
 
 		var headerIcon = document.createElement("i");
-		headerIcon.className = "fas fa-solar-panel";
+		headerIcon.className = "fas fa-fw fa-solar-panel";
 
 		headerDiv.appendChild(headerIcon);
 		headerDiv.appendChild(headerText);
@@ -111,29 +105,36 @@ Module.register("MMM-SolarMan",{
 		var divider = document.createElement("hr");
 		divider.className = "dimmed";
 
+		var graphDiv = document.createElement("div");
+		graphDiv.className = "small thin light";
+		graphDiv.style.width = this.config.width + "px";
+		graphDiv.style.height = (this.config.height-30) /2  + "px";
+		graphDiv.style.display = "table-cell";
 		var graph = document.createElement("canvas");
-		graph.className = "small thin light";
-		graph.style.width = this.config.width + "px";
-		graph.style.height = this.config.height - 30 + "px";
-		graph.style.display = "none";
 		graph.id = "solarmanGraph";
+		graphDiv.appendChild(graph);
 
 		var msg = document.createElement("div");
 		msg.className = "small bright";
 		msg.style.width = this.config.width + "px";
-		msg.style.height = 30 + "px";
+		msg.style.height = (this.config.height-30)/2 + "px";
+		msg.style.display = "table-cell";
 		msg.id = "solarmanStats";
 		msg.innerHTML = this.translate("LOADING");
 
 		container.appendChild(headerDiv);
 		container.appendChild(divider);
 		container.appendChild(msg);
-		container.appendChild(graph);
+		container.appendChild(graphDiv);
 
 		return container;
 	},
   
 	drawSummaryTable:  function(data){
+		console.log("Going to draw summary table: ");
+		console.log(JSON.stringify(data));
+
+
 		var container = document.getElementById("solarmanStats");
 		container.style.display = "block";
 		container.innerHTML = ""; // Clear previous content
@@ -142,16 +143,29 @@ Module.register("MMM-SolarMan",{
 		var table = document.createElement("table");
 		table.className = "small thin light";
 		table.style.width = this.config.width + "px";
-		table.style.height = this.config.height - 30 + "px";
 		table.id = "solarmanStatsTable";
 
-		table.append(document.createElement("tr").innerHTML(`<td class='small regular bright'>System</td><td class='small light bright'>${data.status}</td>`)); 
-		table.append(document.createElement("tr").innerHTML(`<td class='small regular bright'>Load</td><td class='small light bright'>${data.load} W</td>`));
-		table.append(document.createElement("tr").innerHTML(`<td class='small regular bright'>Generating</td><td class='small light bright'>${data.generating} W</td>`));
-		table.append(document.createElement("tr").innerHTML(`<td class='small regular bright'>Battery</td><td class='small light bright'>${data.battery} W</td>`));
-		table.append(document.createElement("tr").innerHTML(`<td class='small regular bright'>Municipality</td><td class='small light bright'>${data.grid} W</td>`));
-		table.append(document.createElement("tr").innerHTML(`<td class='small regular bright'>Battery Status</td><td class='small light bright'>${data.batteryStatus}</td>`));
-		table.append(document.createElement("tr").innerHTML(`<td class='small regular bright'>SOC</td><td class='small light bright'>${data.soc} %</td>`));
+		var row = document.createElement("tr");
+		row.innerHTML = `<td class='small regular bright'>System</td><td class='small light bright'>${data.status}</td>`;
+		table.appendChild(row);
+		row = document.createElement("tr");
+		row.innerHTML = `<td class='small regular bright'>Load</td><td class='small light bright'>${data.load} W</td>`;
+		table.appendChild(row);
+		row = document.createElement("tr");
+		row.innerHTML = `<td class='small regular bright'>Generating</td><td class='small light bright'>${data.generating} W</td>`;
+		table.appendChild(row);
+		row = document.createElement("tr");
+		row.innerHTML = `<td class='small regular bright'>Battery</td><td class='small light bright'>${data.battery} W</td>`;
+		table.appendChild(row);
+		row = document.createElement("tr");
+		row.innerHTML = `<td class='small regular bright'>Municipality</td><td class='small light bright'>${data.grid} W</td>`;
+		table.appendChild(row);
+		row = document.createElement("tr");
+		row.innerHTML = `<td class='small regular bright'>Battery Status</td><td class='small light bright'>${data.batteryStatus}</td>`;
+		table.appendChild(row);
+		row = document.createElement("tr");
+		row.innerHTML = `<td class='small regular bright'>Battery Level</td><td class='small light bright'><div class='progress-container'><div class='progress-bar' style='width:${data.soc}%'>${data.soc}%</div></div></td>`;
+		table.appendChild(row);
 
 		container.appendChild(table);
 
@@ -162,115 +176,127 @@ Module.register("MMM-SolarMan",{
 	* For config options visit https://www.chartjs.org/docs/latest/
 	*/
 	drawDayChart: function(data) {
-		new Chart(document.getElementById("sunnyportalDayGraph"),
+		console.log("Going to draw Month chart with the following data: ");
+		console.log(JSON.stringify(data));
+
+		var container = document.getElementById("solarmanGraph");
+		var ctx = container.getContext("2d");
+		var c = new Chart(ctx,
 		{
 			type: 'line',
 			options:{
-			elements:{
-				point:{
-				pointStyle:false
-				},
-				line:{
-				cubicInterpolationMode:'default',
-				borderWidth:0,
-				},
-				legend:{
-				display: true,
-				position: 'bottom',
-				labels: {
-					color: 'rgb(255,255,255)',
-					boxWidth: 20,
-					usePointStyle: true
-				}
-				},
-			},
-			scales: {
-				x:{
-					ticks:{
-					color: 'rgb(255,255,255)',
-					autoSkip: true,
-					maxRotation: 90,
-					minRotation: 90
-					}
-				},
-				y: {
-					stacked: false,
-					beginAtZero: true,
-					color: 'rgb(255,255,255)',
-					min: 0,
-					max: 8000,
-					title: {
-					display: true,
-					text: 'Power (W)',
-					color: 'rgb(255,255,255)'
+				elements:{
+					point:{
+						pointStyle:false,
+						radius: 0,
 					},
-					ticks: {
-					color: 'rgb(255,255,255)',
-					stepSize: 500,
-					}
-				},
-				y2:{
-					display: true,
-					lineWidth: 0,
-					position: 'right',
-					min:30,
-					max:100,
-					title: {
+					line:{
+						cubicInterpolationMode:'default',
+						borderWidth:0,
+					},
+					legend:{
 						display: true,
-						text: 'Battery %',
-						color: 'rgb(255,255,255)'
+						position: 'bottom',
+						labels: {
+							color: 'rgb(255,255,255)',
+							boxWidth: 20,
+						}
 					},
-					ticks: {
+					
+				},
+				scales: {
+					x:{
+						ticks:{
+							color: 'rgb(255,255,255)',
+							autoSkip: true,
+							maxRotation: 90,
+							minRotation: 90
+						}
+					},
+					y: {
+						stacked: false,
+						beginAtZero: true,
 						color: 'rgb(255,255,255)',
-						stepSize: 5,
+						min: 0,
+						max: 8000,
+						title: {
+							display: true,
+							text: 'Power (W)',
+							color: 'rgb(255,255,255)'
+						},
+						ticks: {
+							color: 'rgb(255,255,255)',
+							stepSize: 500,
+						}
+					},
+					y2:{
+						display: true,
+						lineWidth: 0,
+						position: 'right',
+						min:30,
+						max:100,
+						title: {
+							display: true,
+							text: 'Battery %',
+							color: 'rgb(255,255,255)'
+						},
+						ticks: {
+							color: 'rgb(255,255,255)',
+							stepSize: 5,
+						}
 					}
 				}
-			}
 			},
 			data: {
-			labels: data.map(row => row.dateTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})),
-			datasets:[ 
-			{
-				type: 'line',
-				label: 'SOC',
-				data: data.map(row => row.soc),
-				fill: false,
-				borderWidth: 2,
-				borderColor: 'rgb(255,0,255)',
-				backgroundColor: 'rgb(255,0,255)',
-				yAxisID: 'y2',
-				},
-				{
-				type: 'line',
-				label: 'Grid',
-				data: data.map(row => row.grid),
-				fill: false,
-				borderWidth: 2,
-				borderColor: 'rgb(255,0,0)',
-				backgroundColor: 'rgb(255,0,0)',
-				},
-				{
-				label: 'Load',
-				type:'bar',
-				data: data.map(row => row.load),
-				fill: true,
-				borderColor: 'rgb(0,255,0)',
-				backgroundColor: 'rgb(0,255,0)',
-				},
-				{
-				type: 'line',
-				label: 'Generation',
-				data: data.map(row => row.generationPower),
-				fill: false,
-				borderWidth: 2,
-				borderColor: 'rgb(0,0,255)',
-				backgroundColor: 'rgb(0,0,255)',
-				},          
-			]
+				labels: data.map(row => row.timeLabel),
+				datasets:[ 
+					{
+						type: 'line',
+						label: 'SOC',
+						data: data.map(row => row.soc),
+						fill: false,
+						borderWidth: 2,
+						borderColor: 'rgb(255,0,255)',
+						backgroundColor: 'rgb(255,0,255)',
+						yAxisID: 'y2',
+						pointStyle : false
+					},
+					{
+						type: 'line',
+						label: 'Grid',
+						data: data.map(row => row.grid),
+						fill: false,
+						borderWidth: 2,
+						borderColor: 'rgb(255,0,0)',
+						backgroundColor: 'rgb(255,0,0)',
+						yAxisID: 'y'
+					},
+					{
+						type:'bar',
+						label: 'Load',
+						data: data.map(row => row.load),
+						fill: true,
+						borderColor: 'rgb(0,255,0)',
+						backgroundColor: 'rgb(0,255,0)',
+						yAxisID: 'y',
+						borderWidth: 2,
+						minBarLength: 2,
+						//barThickness: 10,
+						//borderRadius: 5,
+					},
+					{
+						type: 'line',
+						label: 'Generation',
+						data: data.map(row => row.generationPower),
+						fill: false,
+						borderWidth: 2,
+						borderColor: 'rgb(0,0,255)',
+						backgroundColor: 'rgb(0,0,255)',
+						yAxisID: 'y'
+					}         
+				]
 			}
-		}
-		);
-	
+		});
 	}
 	
   });
--- node_helper.js
+++ node_helper.js
@@ -6,17 +6,18 @@
  * MIT Licensed.
  *
  */
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 var NodeHelper = require('node_helper');
-var fetch = require('node-fetch');
+
 
 /**
- * Sunny Portal API Node Library
  * For interfacing with SolarmanPV.
  *
  * @module
  * @param {Object} opts  Need to pass a stationID, token.
  */
-var SolarMan = async function(opts) {
+var SolarMan = async function(opts,source) {
 
 	if(!opts.token) {
 		throw new Error('token');
@@ -25,49 +26,66 @@ var SolarMan = async function(opts) {
 		throw new Error('stationID Must Be Defined');
 	}
 
-	//https://globalhome.solarmanpv.com/maintain-s/history/batteryPower/62052809/stats/daily?year=2025&month=6&day=8
-	var today = new Date();
-	var year = today.year();
-	var month =today.month();
-	var day = today.day();
-	var dataUrl = `globalhome.solarmanpv.com/maintain-s/history/batteryPower/${opts.url}/${opts.stationID}/stats/daily?year=${year}&month=${month}&day=${day}`;
-	var token = opts.token;
-	
-	const response = await fetch(
-	dataUrl,
-	{ headers: { Authorization: `Bearer ${token}`, UserAgent:'MagicMirror' } }
-	);
-	const json = await response.json();
-	const records = json.records;
-	const stats = json.statistics;
-
-	records.forEach(row => {
-		this.chartData.push({   
-		dateTime : new Date((row.dateTime + row.timeZoneOffset) * 1000),
-		timeZoneOffset: row.timeZoneOffset,
-		batteryPower: row.batteryPower,
-		generationPower: row.generationPower,
-		load: row.usePower,
-		soc: row.batterySoc,
-		grid: row.wirePower,
-		});
-	});
-
-	this.statsData = new {
-		status: stats.warningStatus,
-		load: stats.usePower,
-		generating: stats.generationPower,
-		battery: stats.dischargePoser,
-		grid: stats.buyPower,
-		batteryStatus: stats.batteryStatus,
-		soc: stats.batterySoc,
+	var dataUrl = "";
+	const token = opts.token;
+	if (source === "system") {
+		//https://globalhome.solarmanpv.com/maintain-s/operating/system/62052809
+		dataUrl = `https://globalhome.solarmanpv.com/maintain-s/operating/system/${opts.stationID}`;
 	}
+	else {
+		//https://globalhome.solarmanpv.com/maintain-s/history/batteryPower/62052809/stats/daily?year=2025&month=6&day=8
+		var today = new Date();
+		var year = today.getFullYear();
+		var month =today.getMonth();
+		var day = today.getDate();
+		 dataUrl = `https://globalhome.solarmanpv.com/maintain-s/history/batteryPower/${opts.stationID}/stats/daily?year=${year}&month=${month}&day=${day}`;
+	}
+		
+	const response = await fetch(dataUrl,
+		{ headers: { 
+			Authorization: `Bearer ${token}`, 
+			UserAgent:'MagicMirror' } 
+		}, 
+	);
 
-	return {
-		stats : statsData,
-        details : chartData
-	};
+	if (!response.ok) {
+		console.error(`Error url:${dataUrl} status:${response.statusText} body: ${response.text()}`);
+		return null;;
+	} 	 
 
+
+	if (source === "system") {
+		const json = await response.json();
+		this.stats = json;
+		return {
+				status: this.stats.consumerWarningStatus,
+				load: this.stats.usePower,
+				generating: this.stats.generationPower,
+				battery: this.stats.dischargePower,
+				grid: this.stats.wirePower,
+				batteryStatus: this.stats.batteryStatus,
+				soc: this.stats.batterySoc
+			};
+	}
+	else {
+		const json = await response.json();
+		const records = json.records;
+		const chartData = [];
+		
+		records.forEach(row => {
+			chartData.push({   
+				dateTime : new Date((row.dateTime) * 1000),
+				timeLabel: new Date((row.dateTime) * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}),
+				batteryPower: row.batteryPower,
+				generationPower: row.generationPower,
+				load: row.usePower,
+				soc: row.batterySoc,
+				grid: row.wirePower,
+			});
+		});
+	
+		return chartData
+	}
 };
 
 module.exports = NodeHelper.create({
@@ -75,41 +93,46 @@ module.exports = NodeHelper.create({
     start: function() {
 	  console.log("Starting node helper for: " + this.name);
 	  this.started = false;
+	  this.config = {};
       return;
     },
 
 	// Override socketNotificationReceived method.
-	socketNotificationReceived: function(notification, payload) {
+	socketNotificationReceived: async function(notification, payload) {
 
 		// We save this.started (update timer) status, to prevent mutiple timer restarts
         // for each new client connection/instance.
 		var self = this;
-
+        //console.debug("MMM-SolarMan: Node helper received notification: " + notification);
+		//console.debug("MMM-SolarMan: Node helper received payload: " + JSON.stringify(payload));
 		if (notification === "START_SOLARMAN" && this.started == false) {				
 			console.log("SocketNotification START_SOLARMAN received for the first time...setting updateInterval to " + payload.updateInterval + "ms");
-			startup(payload); // When the MagicMirror module is called the first time, we are immediatly going to fetch data
+			self.processData(payload); // When the MagicMirror module is called the first time, we are immediatly going to fetch data
    			setInterval(function() { processData(payload) }, payload.updateInterval); // Now let's schedule the job
 			self.started = true;
 		} else if (notification === "START_SOLARMAN" && this.started == true) {
 			console.log("SocketNotification START_SOLARMAN received");
-			self.processData(payload);
+			await self.processData(payload);
 		}
-  },
+		return;
+	},
 
-  processData: function(payload) {
-	  const pv = new SolarMan(payload.url,payload.stationID, payload.token);
-	  console.log("Starting function processDayData with data: " + pv);
-
-    // Send all to script
-    self.sendSocketNotification('SOLARMAN_DAY_SUMMARY', {
-		payload: payload,
-        data: pv.stats
-    });
-
-	self.sendSocketNotification('SOLARMAN_DAY_DETAIL',{
-		payload: payload,
-		data: pv.details
-	});
-  },
+	processData: async function(payload) {
+		var self = this;
+		
+		// Send all to script
+		const pv = await SolarMan(payload,"system");
+		self.sendSocketNotification('SOLARMAN_DAY_SUMMARY', {
+			payload: payload,
+			data: pv
+		});
+	
+		const pv2 = await SolarMan(payload,"detail");
+		self.sendSocketNotification('SOLARMAN_DAY_DETAIL',{
+			payload: payload,
+			data: pv2
+		});
+		return;
+	},
 
 });
